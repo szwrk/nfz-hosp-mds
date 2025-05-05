2906701af59751c812da71fca2be7a292f93d4a4
Arek <arek@wilamowski.net>
2025-05-05 21:39:34 +0200

    chore: autorender tree

---
+++ app/pipeline/extract.py
@@ -0,0 +1,22 @@
+﻿from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .master("local[*]") \
+    .getOrCreate()
+
+jdbc_url = "jdbc:postgresql://192.168.0.102:5432/datamart"
+properties = {
+    "user": "sysdwh",
+    "password": "oracle",
+    "driver": "org.postgresql.Driver"
+}
+
+try:
+    df = spark.read.jdbc(url=jdbc_url, table="(SELECT current_date) AS test", properties=properties)
+    df.show()
+except Exception as e:
+    print("❌ Connection failed:", e)
+else:
+    print("✅ Connection successful")
+finally:
+    spark.stop()
